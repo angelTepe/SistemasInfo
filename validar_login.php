2dcf9825654c4e65800a05718f0cf9b3c1fbb6c9
angelTepe <56660871+angelTepe@users.noreply.github.com>
2022-06-02 16:00:24 -0500

    Update validar_login.php

--- validar_login.php
+++ validar_login.php
@@ -4,10 +4,13 @@ include "conexion.php";
 $usuario = $_POST['nombre_usuario'];
 $password = $_POST['password'];
 
-$q = "SELECT COUNT(*) as contar from usuarios where nombre_usuario= '$usuario' and password = '$password' and permisos= '2' ";
+$q = "SELECT COUNT(*) as contar from usuarios where nombre_usuario= '$usuario' and  password = '$password' and Permisos=2 ";
 $consulta = mysqli_query($conexion,$q);
+$qdos = "SELECT COUNT(*) as contar2 from usuarios where nombre_usuario= '$usuario' and password = '$password' ";
+$consultados = mysqli_query($conexion,$qdos);
 
 $array = mysqli_fetch_array($consulta);
+$arraydos = mysqli_fetch_array($consultados);
 
 if($array['contar']>0){
     
@@ -15,9 +18,12 @@ if($array['contar']>0){
     $_SESSION['usermane']=$usuario;
 
     header("location: ./dashboard.php");
-}else{
-
+}else if($arraydos['contar2']>0){
+    //Aqui va para usuario
+    $_SESSION['usermane']=$usuario;
+    header("location: ./dashboard_usuario.php");
+}
+else{
     header("location: ./login_error.php");
 }
-
 ?>
